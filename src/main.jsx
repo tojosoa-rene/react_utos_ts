27ca7ac3fefc418b5d801a19fc0f2388846614ee
elodieetiennette1 <elodieetiennette11@gmail.com>
2026-04-14 08:52:42 +0400

    bootstrap package added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 import App from './App.jsx'
